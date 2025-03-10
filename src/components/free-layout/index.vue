f12b206eb8782da21993d260908b5d80dc858a95
heyongkui <heyongkui@baiwang.com>
2025-03-10 13:34:21 +0800

    fix: 修改free-layout2

--- src/components/free-layout/index.vue
+++ src/components/free-layout/index.vue
@@ -1,21 +1,31 @@
 <template>
     <table class="yk-free-layout" :style="{ borderSpacing: borderSpacing }">
+        <tr>
+            <td class="hidden-fix hidden-fix-row" v-for="(col, index) in tableRenderData[0]" :key="'hidden' + index">
+            </td>
+        </tr>
         <tr class="yk-free-layout-tr" v-for="(rowTds, index) in tableRenderData" :key="'row:' + index">
+            <td class="hidden-fix hidden-fix-col"></td>
             <td class="yk-free-layout-td" v-for="col in rowTds.filter(item => item._show)" :key="col._key" :rowspan="col.rowspan" :colspan="col.colspan">
                 <div class="yk-free-layout-td-wrapper">
                     <div class="yk-free-layout-td-toolbar">
-                        <div class="label">行：</div>
-                        <div class="input">
-                            <input type="number" min="1" :max="col._rowspanMax" v-model="col.rowspan" @input="rowspanChange($event.target.value, col)">
+                        <div class="yk-free-layout-td-toolbar-content">
+                            <div class="label">行：</div>
+                            <div class="control">
+                                <input class="input" type="number" min="1" :max="col._rowspanMax" v-model="col.rowspan" @input="rowspanChange($event.target.value, col)">
+                            </div>
+                            <div class="label">列：</div>
+                            <div class="control">
+                                <input class="input" type="number" min="1" :max="col._colspanMax" v-model="col.colspan" @input="colspanChange($event.target.value, col)">
+                            </div>
                         </div>
-                        <div class="label">列：</div>
-                        <div class="input">
-                            <input type="number" min="1" :max="col._colspanMax" v-model="col.colspan" @input="colspanChange($event.target.value, col)">
-                        </div>
+                        <div class="yk-free-layout-td-toolbar-icon"></div>
                     </div>
-                    <span>
-                        {{ col._key }}
-                    </span>
+                    <div class="yk-free-layout-td-content">
+                        {{ col._key }}<br>
+                        _rowspanMax: {{ col._rowspanMax }}<br>
+                        _colspanMax：{{col._colspanMax}}
+                    </div>
                 </div>
             </td>
         </tr>
@@ -55,10 +65,15 @@ watch(props.rowNum, (newVal, oldVal) => {
     generateTableData()
 }, { immediate: true })
 // 根据行数和列数初始化生成table渲染数据
-function generateTableData () {
+function generateTableData (rowNum, colNum) {
+    rowNum = rowNum || props.rowNum
+    colNum = colNum || props.colNum
+    // 清空表格数据
     tableRenderData.length = 0
+    // 遍历行
     for (const row of createArrayWithNum(props.rowNum)) {
         const rowData = reactive([])
+        // 遍历列
         for (const col of createArrayWithNum(props.colNum)) {
             rowData.push(reactive({
                 _rowIndex: row,
@@ -71,11 +86,14 @@ function generateTableData () {
                 oldRowspan: 1,
                 colspan: 1,
                 oldColspan: 1,
-                component: {},
+                // components: reactive([]), // 单元格内部放置的组件，多个，可调整层级（可编辑），可控制是否撑满展示
             }))
         }
         tableRenderData.push(rowData)
     }
+}
+function reset () {
+    generateTableData()
 }
 // 合并行数据改变
 function rowspanChange (val, col) {
@@ -113,8 +131,8 @@ function spanDataHandler (val, cell, type) {
     const spanDiff = newSpanVal - oldSpanVal
     const spanAbsDiff = Math.abs(spanDiff)
     const flag = spanDiff < 0
-    const effectRowIndexArr = createArrayWithNum(type === 'row' ? spanAbsDiff : cell.rowspan) 
-    const effectColIndexArr = createArrayWithNum(type === 'row' ? cell.colspan: spanAbsDiff)
+    const effectRowIndexArr = type === 'row' ? createArrayWithNum(spanAbsDiff, 1) : createArrayWithNum(cell.rowspan) 
+    const effectColIndexArr = type === 'row' ? createArrayWithNum(cell.colspan) : createArrayWithNum(spanAbsDiff, 1)
     console.log(type + ' effectColIndexArr : ', effectColIndexArr);
     console.log(type + ' effectRowIndexArr : ', effectRowIndexArr);
     console.log(type + ' spanDiff : ', spanDiff);
@@ -133,15 +151,11 @@ function spanDataHandler (val, cell, type) {
     // 记录上次行或者列合并的数据
     cell[oldSpankey] = cell[spanKey]
 }
-// 根据数字创建数组  入参 5 返回 [1,2,3,4,5]
-function createArrayWithNum (n) {
-    n = Math.max(n, 1)
-    return Array.from({length: n}, (_, index) => index)
+// 根据数字创建数组  入参 5 返回 [0,1,2,3,4]  startIndex 控制开始的下标，默认从0开始
+function createArrayWithNum (n, startIndex = 0) {
+    return Array.from({ length: n }, (_, index) => index + startIndex)
 }
-
-
 </script>
-
 <style lang="scss" scoped>
 $border-color: #CCCCCC;
 .yk-free-layout {
@@ -154,9 +168,71 @@ $border-color: #CCCCCC;
     .yk-free-layout-td {
         position: relative;
         border: 1px dashed $border-color;
+        overflow: hidden;
         border-radius: 8px;
         color: red;
         text-align: center;
+        
+        .yk-free-layout-td-toolbar {
+            position: absolute;
+            top: 0;
+            z-index: 999;
+            background-color: #FFF;
+            width: 100%;
+            transform: translateY(-100%);
+            transition: transform .3s ease-in-out;
+            .yk-free-layout-td-toolbar-content {
+                display: flex;
+                padding: 3px 12px;
+                align-items: center;
+                font-size: 12px;
+                line-height: 1.2;
+                .label {
+                    display: inline-block;
+                    padding: 0 3px;
+                    font-size: 12px;
+                }
+                .control {
+                    display: flex;
+                    align-items: center;
+                    .input {
+                        width: auto;
+                        outline: none;
+                        padding: 3px 6px;
+                        border: 1px solid #E6E6E6;
+                        border-radius: 999px;
+                        
+                    }
+                }
+            }
+            .yk-free-layout-td-toolbar-icon {
+                position: absolute;
+                right: 0;
+                bottom: 0;
+                transform: translate(10%, 100%);
+                width: 0;
+                height: 0;
+                border-left: 10px solid transparent;
+                border-right: 10px solid transparent;
+                border-top: 10px solid red;
+                cursor: pointer;
+                margin: 0 auto;
+            }
+            &:hover {
+                transform: translateY(0);
+                box-shadow: 0 2px 1px 0px #f6f6f6;
+            }
+        }
+        .yk-free-layout-td-content {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+        }
+        &:hover {
+
+        }
     }
 }
 </style>
